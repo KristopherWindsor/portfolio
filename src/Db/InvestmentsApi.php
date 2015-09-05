107169f18969840cc9ea32b4e11a609185d6420c
Kristopher Windsor <kristopher@pinger.com>
2015-09-04 23:10:21 -0700

    correct sql for db change in prev commit

--- src/Db/InvestmentsApi.php
+++ src/Db/InvestmentsApi.php
@@ -19,7 +19,11 @@ class InvestmentsApi {
 
   public static function getInvestments($mysqli, $start_year, $end_year, $start_month = 1, $end_month = 12){
     $stmt = $mysqli->prepare("
-      SELECT * FROM `investments`
+      SELECT
+        *,
+        `value_ret_pretax` + `value_ret_posttax` AS `value_ret`,
+        `value_ret_pretax` + `value_ret_posttax` + `value_no_ret` AS `value`
+      FROM `investments`
       LEFT JOIN `investment_category` ON `category_key` = `key`
       WHERE
         (`year` > ? OR (`year` = ? AND `month` >= ?)) AND
@@ -35,23 +39,4 @@ class InvestmentsApi {
     return $res;
   }
 
-  /*
-  public static function addProduct($mysqli, $name, $dimensions, $weight){
-    $stmt = $mysqli->prepare("INSERT INTO `product` (`name`, `dimensions`, `weight`) VALUES (?, ?, ?)");
-    $stmt->bind_param('sss', $name, $dimensions, $weight);
-    $stmt->execute();
-    if ($stmt->affected_rows <= 0)
-      throw new \Exception('Cannot add product, it probably already exists');
-    $stmt->close();
-  }
-
-  public static function getProduct($mysqli, $name){
-    $stmt = $mysqli->prepare("SELECT * FROM `product` WHERE `name` = ?");
-    $stmt->bind_param('s', $name);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    while ($row = $result->fetch_object())
-      return $row;
-    throw new \Exception('Product not found');
-  }*/
 }
