2d368e663aec326eeb5d6a9a74737a4d879bbfe8
Kristopher Windsor <kristopher@pinger.com>
2016-10-06 20:39:02 -0700

    run mysql then apache, derp

--- scripts/dockerInit.sh
+++ scripts/dockerInit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apache2-foreground
-
 # This file has the data we want to show
 mysql -h $DB_PORTFOLIO_HOST --port $DB_PORTFOLIO_PORT -u $DB_PORTFOLIO_USER -p$DB_PORTFOLIO_PW -D $DB_PORTFOLIO_DB < /var/www/scripts/kristopher_data.sql
+
+apache2-foreground
