e9590953c951975b739f4edd80bb0942a47878a6
Kristopher Windsor <kristopher@pinger.com>
2016-10-06 20:22:53 -0700

    try something else again..

--- scripts/dockerInit.sh
+++ scripts/dockerInit.sh
@@ -3,4 +3,4 @@
 apache2-foreground
 
 # This file has the data we want to show
-CMD mysql -h $DB_PORTFOLIO_HOST --port $DB_PORTFOLIO_PORT -u $DB_PORTFOLIO_USER -p$DB_PORTFOLIO_PW -D $DB_PORTFOLIO_DB < /var/www/scripts/kristopher_data.sql
+mysql -h $DB_PORTFOLIO_HOST --port $DB_PORTFOLIO_PORT -u $DB_PORTFOLIO_USER -p$DB_PORTFOLIO_PW -D $DB_PORTFOLIO_DB < /var/www/scripts/kristopher_data.sql
